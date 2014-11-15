d9ce19952edbd2243776dba5966940b5e1c50337
Valentin Druon <valentin.druon@gmail.com>
2014-11-15 10:10:45 +0100

    Get usual metadata

--- lib/spider-seo.rb
+++ lib/spider-seo.rb
@@ -1,55 +1,43 @@
-require "spider/seo/version"
+require "spider-seo/version"
 require 'nokogiri'
+require 'spider-seo/metadata'
 
 module SpiderSeo
   class Document
+    # Contains Nokogiri::HTML::Document object
+    attr_accessor :document
+    attr_accessor :metadata
 
-    private
-      # Writer for :document
-      attr_writer :document
-      # Writer for :metadata
-      attr_writer :metadata
-
-    public
-      # Contains Nokogiri::HTML::Document object
-      attr_reader :document
-      # Contains a SpiderSeo::Document::Metadata object
-      attr_reader :metadata
-
-      # Getter for metadata
-      def metadata
-        self.metadata ||= SpiderSeo::Document::Metadata.new
-      end
-
-      # Initializer
-      # uri_or_html is either an URI or a HTML string
-      # options is options to pass to open-uri#open
-      def initialize uri_or_html = nil, options = {}
-        if uri_or_html
-          require 'uri'
-          if uri_or_html ~= URI::regexp # Use URI::regexp to check if uri_or_html is an uri
-            self.document = from_uri uri_or_html
-          elsif uri_or_html =~ /^s*<[^Hh>]*html/ # Use regexp to check if uri_or_html is a HTML string
-            self.document = from_html uri_or_html, options
-          else
-            raise "The string you provided is neither an URI nor a HTML string."
-          end
+    # Initializer
+    # uri_or_html is either an URI or a HTML string
+    # options is options to pass to open-uri#open
+    def initialize uri_or_html = nil, options = {}
+      if uri_or_html
+        require 'uri'
+        if uri_or_html =~ /\A#{URI::regexp}\z/ # Use URI::regexp to check if uri_or_html is an uri
+          self.document = from_uri uri_or_html
+        elsif uri_or_html =~ /^s*<[^Hh>]*html/ # Use regexp to check if uri_or_html is a HTML string
+          self.document = from_html uri_or_html, options
+        else
+          raise "The string you provided is neither an URI nor a HTML string."
         end
-        return self
+        self.metadata = SpiderSeo::Document::Metadata.new(self.document)
       end
+      return self
+    end
 
-      # Returns a Nokogiri::HTML::Document
-      # From a HTML string
-      def from_html html
-        self.document = Nokogiri::HTML(html)
-      end
+    # Returns a Nokogiri::HTML::Document
+    # From a HTML string
+    def from_html html
+      self.document = Nokogiri::HTML(html)
+    end
 
-      # Returns a Nokogiri::HTML::Document
-      # From an uri
-      # options are options available for open_uri#open
-      def from_uri uri, options = {}
-        require 'open-uri'
-        self.document = Nokogiri::HTML(open(uri, options))
-      end
+    # Returns a Nokogiri::HTML::Document
+    # From an uri
+    # options are options available for open_uri#open
+    def from_uri uri, options = {}
+      require 'open-uri'
+      self.document = Nokogiri::HTML(open(uri, options))
+    end
   end
 end
--- lib/spider-seo/metadata.rb
+++ lib/spider-seo/metadata.rb
@@ -1,5 +1,92 @@
 module SpiderSeo
   class Document
-    
+    class Metadata
+
+      # Initializer
+      # param document is a Nokogiri::HTML::Document object
+      def initialize doc
+        self.document = doc
+      end
+
+      attr_accessor :document
+
+      # Getter for title
+      def title
+        self.document.title
+      end
+
+      # Getter for encoding
+      def encoding
+        self.document.meta_encoding
+      end
+
+      # Getter for meta keywords
+      def keywords options = {}
+        xpath_meta_query('keywords')
+      end
+
+      # Getter for meta description
+      def description
+        xpath_meta_query('description')
+      end
+
+      # Getter for meta google
+      def google
+        xpath_meta_query('google')
+      end
+
+      # Getter for meta googlebot
+      def googlebot
+        xpath_meta_query('googlebot')
+      end
+
+      # Getter for meta robots
+      def robots
+        xpath_meta_query('robots')
+      end
+
+      # Getter for meta verify
+      def verify
+        xpath_meta_query('verify')
+      end
+
+      # Getter for a specific meta tag
+      # No cache for this method, Nokogiri::HTML::Document#xpath method is executed on each method call
+      # param attr_name is meta attribute name (like name, http-equiv, property, ...)
+      # param attr_value is meta attribute value (such as keywords, description, robots, ...)
+      def meta(attr_value, attr_name = nil)
+        return advanced_xpath_meta_query(attr_name, attr_value) if attr_name
+        xpath_meta_query(attr_value)
+      end
+
+      # Getter for all meta tags that have name or http-equiv attribute
+      # Returns a hash where
+      # key = name|http-equiv meta tag attribute
+      # value = content meta tag attribute
+      def all
+        self.document.xpath("//meta").map { |node| Hash[node.attribute_nodes.map { |att| [att.node_name, att.value] } ] }
+      end
+
+      private
+        # self.document accessor
+
+        # xPath query for metadata
+        XPATH_META_QUERY = "//meta[translate(@%{attr_name},'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz') ='%{attr_value}']/@content"
+
+        # xPath query executor for meta tags
+        # Will only look for tag that have name="#{name_or_http_equiv_value}" or http-equiv="#{name_or_http_equiv_value}"
+        # Translate in xpath query is here to handle meta tags case (ex: name="keywords" or name="KEyWordS" etc...) and lower-case it
+        def xpath_meta_query(name_or_http_equiv_value)
+          (self.document.xpath(XPATH_META_QUERY % {attr_name: 'name', attr_value: name_or_http_equiv_value.downcase}) ||
+          self.document.xpath(XPATH_META_QUERY % {attr_name: 'http-equiv', attr_value: name_or_http_equiv_value.downcase})).to_s
+        end
+
+        # xPath advances query executor for meta tags
+        # More precise than #xpath_meta_query, because you can pass attr_name which will enable looking for
+        # tags such as <meta #{attr_name}="#{attr_value}"
+        def advanced_xpath_meta_query(attr_name, attr_value)
+          self.document.xpath(XPATH_META_QUERY % { attr_name: attr_name, attr_value: attr_value }).to_s
+        end
+    end
   end
 end
