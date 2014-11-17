1015b2ddc9b2ae90fd4190359d862009ac4c65e4
Valentin Druon <valentin.druon@gmail.com>
2014-11-17 17:10:24 +0100

    Added :text to SpiderSeo::Document::Tag

--- lib/spider-seo/tag.rb
+++ lib/spider-seo/tag.rb
@@ -2,11 +2,13 @@ module SpiderSeo
   class Document
     class Tag
       attr_accessor :name
+      attr_accessor :text
       attr_accessor :attributes
 
       # Constructor
-      def initialize name = nil, attributes = []
+      def initialize name = nil, text = nil, attributes = []
         self.name = nil
+        self.text = text
         self.attributes = attributes
       end
     end
